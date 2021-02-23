60378277f814b630c827e0c912b3b18fd52098fc
vijay-mathur <vijaymathur948@gmail.com>
2021-02-23 17:33:37 +0530

    Updates

---
+++ precache-manifest.35637e16d473f589ee8c25f289ff1401.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98a30b371d9f470e917ab7871cdd163",
+    "url": "/vijaymathur948.github.io/myCssClasses/index.html"
+  },
+  {
+    "revision": "9d722870aa0aaf48ebdd",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "5515004b716f0f96aa77",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/css/main.1d641934.chunk.css"
+  },
+  {
+    "revision": "9d722870aa0aaf48ebdd",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/js/2.7ea6bc0b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/js/2.7ea6bc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5515004b716f0f96aa77",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/js/main.aa4e7b29.chunk.js"
+  },
+  {
+    "revision": "ceb39284f4d63cd2e3b5",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/js/runtime-main.ce6eb8a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vijaymathur948.github.io/myCssClasses/static/media/logo.5d5d9eef.svg"
+  }
+]);
